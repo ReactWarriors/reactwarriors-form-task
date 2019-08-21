fc1ea02ca2bbefd1134ae552be9958dcd894691e
Denis Goncearuc <goncearuc17@gmail.com>
2019-08-21 05:01:01 +0200

    onSubmit state.values desturcturization added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -51,38 +51,28 @@ export default class App extends React.Component {
   onSubmit = e => {
     e.preventDefault()
     const errors = {}
+    const values = this.state.values
     switch (this.state.currentStep) {
       case 1:
-        if (this.state.values.firstName.length < 5)
+        if (values.firstName.length < 5)
           errors.firstName = 'Must be 5 characters or more'
-        if (this.state.values.lastName < 5)
+        if (values.lastName < 5)
           errors.lastName = 'Must be 5 characters or more'
-        if (this.state.values.password < 6)
+        if (values.password < 6)
           errors.password = 'Must be 6 characters or more'
-        if (
-          this.state.values.password !== this.state.values.repeatPassword ||
-          this.state.values.password === ''
-        )
+        if (values.password !== values.repeatPassword || values.password === '')
           errors.repeatPassword = 'Must be equal password'
         break
       case 2:
-        if (
-          !this.state.values.email.includes('@') ||
-          this.state.values.email.length < 8
-        )
+        if (!values.email.includes('@') || values.email.length < 8)
           errors.email = 'Required'
-        if (
-          !this.state.values.phone.includes('+') ||
-          this.state.values.email.length < 9
-        )
+        if (!values.phone.includes('+') || values.email.length < 9)
           errors.phone = 'Required'
-        if (this.state.values.country === '')
-          errors.country = 'No selected country'
-        if (this.state.values.city === '') errors.city = 'No selected city'
+        if (values.country === '') errors.country = 'No selected country'
+        if (values.city === '') errors.city = 'No selected city'
         break
       case 3:
-        if (this.state.values.avatar === '')
-          errors.avatar = 'Choose your avatar please'
+        if (values.avatar === '') errors.avatar = 'Choose your avatar please'
         break
       default:
     }
