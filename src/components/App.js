47fe9d40934624dd39b633ae109e88ee0bc57d95
AntonChetveryk <chetverykanton92@gmail.com>
2019-11-13 11:11:11 +0200

    add project

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,39 @@
 import React from "react";
+import Field from "./Field";
 
-export default class App extends React.Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      page: 1
+    };
+  }
+
+  onClick = event => {
+    event.preventDefault();
+    console.log("click");
+  };
+
   render() {
     return (
-      <div className="form-container card">
-        <form className="form card-body" />
+      <div className="container">
+        <form className="form form_one">
+          <div className="tabs_container">
+            <div className="tabs_container_item">1</div>
+            <div className="tabs_container_item">2</div>
+            <div className="tabs_container_item">3</div>
+            <div className="tabs_container_item">4</div>
+          </div>
+          <Field />
+          <div className="button_container">
+            <button onClick={this.onClick}>Previous</button>
+            <button onClick={this.onClick}>Next</button>
+          </div>
+          <div className="page">{`page:  ${this.state.page}`}</div>
+        </form>
       </div>
     );
   }
 }
+
+export default App;
---
+++ src/components/Field.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Field = props => {
+  return (
+    <div className="form-group">
+      <label>Anton</label>
+      <input></input>
+    </div>
+  );
+};
+
+export default Field;
